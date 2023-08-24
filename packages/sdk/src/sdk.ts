4cca8df693dacc40374d6f620b17080aad39b768
Andrei <t.fane@mail.ru>
2023-08-24 22:52:44 +0700

    fix: add comments

--- packages/sdk/src/sdk.ts
+++ packages/sdk/src/sdk.ts
@@ -8,7 +8,9 @@ export class LidoSDK {
   readonly staking: LidoSDKStaking;
 
   constructor(props: LidoSDKCoreProps) {
+    // Core functionality
     this.core = new LidoSDKCore(props, version);
+    // Staking functionality
     this.staking = new LidoSDKStaking({ ...props, core: this.core });
   }
 }
