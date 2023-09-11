891f88118d7e4b1fb9be5f3ddcc683e5f37f9c0a
Andrei <t.fane@mail.ru>
2023-09-11 21:31:30 +0700

    feat: add callback for claim

--- packages/sdk/src/withdrawals/claim/claim.ts
+++ packages/sdk/src/withdrawals/claim/claim.ts
@@ -2,6 +2,7 @@ import invariant from 'tiny-invariant';
 
 import { type LidoSDKCoreProps } from '../../core/index.js';
 import { Logger, Cache, ErrorHandler } from '../../common/decorators/index.js';
+import { TransactionCallbackStage } from '../../core/types.js';
 import { version } from '../../version.js';
 
 import { Bus } from '../bus.js';
@@ -34,9 +35,11 @@ export class LidoSDKWithdrawalsClaim {
   @Logger('Call:')
   @ErrorHandler('Error:')
   public async claimRequestsEOA(props: ClaimRequestsProps) {
-    const { account, requestsIds, hints } = props;
+    const { account, requestsIds, hints, callback } = props;
 
     const contract = await this.bus.contract.getContractWithdrawalsQueue();
+
+    callback?.({ stage: TransactionCallbackStage.GAS_LIMIT });
 
     const feeData = await this.bus.core.getFeeData();
     const gasLimit = await this.claimGasLimit(props);
@@ -46,11 +49,18 @@ export class LidoSDKWithdrawalsClaim {
       maxFeePerGas: feeData.maxFeePerGas ?? undefined,
     };
 
+    callback?.({ stage: TransactionCallbackStage.SIGN, payload: gasLimit });
+
     const params = [requestsIds, hints] as const;
     const transaction = await contract.write.claimWithdrawals(params, {
       ...overrides,
       gas: gasLimit,
       chain: this.bus.core.chain,
+    });
+
+    callback?.({
+      stage: TransactionCallbackStage.RECEIPT,
+      payload: transaction,
     });
 
     const transactionReceipt =
@@ -58,10 +68,20 @@ export class LidoSDKWithdrawalsClaim {
         hash: transaction,
       });
 
+    callback?.({
+      stage: TransactionCallbackStage.CONFIRMATION,
+      payload: transactionReceipt,
+    });
+
     const confirmations =
       await this.bus.core.rpcProvider.getTransactionConfirmations({
         hash: transactionReceipt.transactionHash,
       });
+
+    callback?.({
+      stage: TransactionCallbackStage.DONE,
+      payload: confirmations,
+    });
 
     return { hash: transaction, receipt: transactionReceipt, confirmations };
   }
@@ -69,24 +89,19 @@ export class LidoSDKWithdrawalsClaim {
   @Logger('Call:')
   @ErrorHandler('Error:')
   public async claimRequestsMultisig(props: ClaimRequestsProps) {
-    const { account, requestsIds, hints } = props;
+    const { account, requestsIds, hints, callback } = props;
 
     const contract = await this.bus.contract.getContractWithdrawalsQueue();
 
-    const feeData = await this.bus.core.getFeeData();
-    const gasLimit = await this.claimGasLimit(props);
-    const overrides = {
-      account,
-      maxPriorityFeePerGas: feeData.maxPriorityFeePerGas ?? undefined,
-      maxFeePerGas: feeData.maxFeePerGas ?? undefined,
-    };
+    callback?.({ stage: TransactionCallbackStage.SIGN });
 
     const params = [requestsIds, hints] as const;
     const transaction = await contract.write.claimWithdrawals(params, {
-      ...overrides,
-      gas: gasLimit,
+      account,
       chain: this.bus.core.chain,
     });
+
+    callback?.({ stage: TransactionCallbackStage.MULTISIG_DONE });
 
     return { hash: transaction };
   }
--- packages/sdk/src/withdrawals/claim/types.ts
+++ packages/sdk/src/withdrawals/claim/types.ts
@@ -1,7 +1,10 @@
 import { type Address } from 'viem';
 
+import { TransactionCallback } from '../../core/index.js';
+
 export type ClaimRequestsProps = {
   account: Address;
   requestsIds: bigint[];
   hints: bigint[];
+  callback?: TransactionCallback;
 };
