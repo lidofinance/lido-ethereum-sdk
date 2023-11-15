2d3859e76aa5fe9d2984df478c23eb4b2e88459a
Evgeny Taktarov <je.mouth@gmail.com>
2023-11-15 19:14:04 +0700

    fix: build error

--- packages/sdk/src/events/events.ts
+++ packages/sdk/src/events/events.ts
@@ -1,12 +1,12 @@
 import { LidoSDKModule } from '../common/class-primitives/sdk-module.js';
+import { LidoSDKCommonProps } from '../core/types.js';
 
 import { LidoSDKStethEvents } from './steth-events.js';
-import type { LidoSDKEventsProps } from './types.js';
 
 export class LidoSDKEvents extends LidoSDKModule {
   readonly stethEvents: LidoSDKStethEvents;
 
-  constructor(props: LidoSDKEventsProps) {
+  constructor(props: LidoSDKCommonProps) {
     super(props);
 
     this.stethEvents = new LidoSDKStethEvents({ ...props, core: this.core });
