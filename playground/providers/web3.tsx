31c241aa5ebdb26a1aa81a2bbd194e8e9e3e0753
Evgeny Taktarov <je.mouth@gmail.com>
2023-12-06 17:13:10 +0700

    test: fix build and custom rpc

--- playground/providers/web3.tsx
+++ playground/providers/web3.tsx
@@ -1,63 +1,124 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { FC, PropsWithChildren } from 'react';
+import {
+  FC,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useMemo,
+  useState,
+} from 'react';
+import { dynamics } from 'config';
 import { ProviderWeb3 } from '@reef-knot/web3-react';
 import { getConnectors, holesky } from '@reef-knot/core-react';
-import { backendRPC, getBackendRPCPath, dynamics } from 'config';
 import { WagmiConfig, createClient, configureChains, Chain } from 'wagmi';
 import * as wagmiChains from 'wagmi/chains';
 import { getStaticRpcBatchProvider } from '@lido-sdk/providers';
+import invariant from 'tiny-invariant';
+import { CHAINS } from '@lido-sdk/constants';
 
 const wagmiChainsArray = Object.values({ ...wagmiChains, holesky });
 const supportedChains = wagmiChainsArray.filter((chain) =>
   dynamics.supportedChains.includes(chain.id),
 );
+
+// Adding Mumbai as a temporary workaround
+// for the wagmi and walletconnect bug, when some wallets are failing to connect
+// when there are only one supported network, so we need at least 2 of them.
+// Mumbai should be the last in the array, otherwise wagmi can send request to it.
+// TODO: remove after updating wagmi to v1+
+supportedChains.push(wagmiChains.polygonMumbai);
+
 const defaultChain = wagmiChainsArray.find(
   (chain) => chain.id === dynamics.defaultChain,
 );
 
-const jsonRcpBatchProvider = (chain: Chain) => ({
-  provider: () =>
-    getStaticRpcBatchProvider(
-      chain.id,
-      getBackendRPCPath(chain.id),
-      undefined,
-      12000,
-    ),
-  chain,
-});
+type CustomRpcContextValue = {
+  customRpc: Record<number, string | null>;
+  activeRpc: Record<number, string>;
+  setCustomRpcUrl: (chain: number, rpcUrl: string | null) => void;
+};
 
-const { chains, provider, webSocketProvider } = configureChains(
-  supportedChains,
-  [jsonRcpBatchProvider],
-);
+const CustomRpcContext = createContext<CustomRpcContextValue | null>(null);
+CustomRpcContext.displayName = 'CustomRpcContext';
 
-const connectors = getConnectors({
-  chains,
-  defaultChain,
-  rpc: backendRPC,
-  walletconnectProjectId: dynamics.walletconnectProjectId,
-});
+export const useCustomRpc = () => {
+  const context = useContext(CustomRpcContext);
+  invariant(context);
+  return context;
+};
 
-const client = createClient({
-  connectors,
-  autoConnect: true,
-  provider,
-  webSocketProvider,
-});
+const Web3Provider: FC<PropsWithChildren> = ({ children }) => {
+  const [customRpc, setCustomRpc] = useState<Record<number, string | null>>({});
 
-const Web3Provider: FC<PropsWithChildren> = ({ children }) => (
-  <WagmiConfig client={client}>
-    {/* @ts-ignore */}
-    <ProviderWeb3
-      pollingInterval={1200}
-      defaultChainId={dynamics.defaultChain}
-      supportedChainIds={dynamics.supportedChains}
-      rpc={backendRPC}
-      walletconnectProjectId={dynamics.walletconnectProjectId}
-    >
-      {children}
-    </ProviderWeb3>
-  </WagmiConfig>
-);
+  const activeRpc: Record<number, string> = useMemo(() => {
+    const getRpc = (chain: number): string =>
+      customRpc[chain] ?? dynamics.rpcProviderUrls[chain];
+    return {
+      [CHAINS.Mainnet]: getRpc(CHAINS.Mainnet),
+      [CHAINS.Goerli]: getRpc(CHAINS.Goerli),
+      [CHAINS.Holesky]: getRpc(CHAINS.Holesky),
+    };
+  }, [customRpc]);
+
+  const customRpcContextValue = useMemo(
+    () => ({
+      customRpc,
+      activeRpc,
+      setCustomRpcUrl: (chain: number, rpcUrl: string | null) => {
+        setCustomRpc((old) => ({ ...old, [chain]: rpcUrl }));
+      },
+    }),
+    [customRpc, activeRpc],
+  );
+
+  const client = useMemo(() => {
+    const jsonRcpBatchProvider = (chain: Chain) => ({
+      provider: () =>
+        getStaticRpcBatchProvider(
+          chain.id,
+          activeRpc[chain.id],
+          undefined,
+          12000,
+        ),
+      chain,
+    });
+
+    const { chains, provider, webSocketProvider } = configureChains(
+      supportedChains,
+      [jsonRcpBatchProvider],
+    );
+
+    const connectors = getConnectors({
+      chains,
+      defaultChain,
+      rpc: activeRpc,
+      walletconnectProjectId: dynamics.walletconnectProjectId,
+    });
+
+    return createClient({
+      connectors,
+      autoConnect: true,
+      provider,
+      webSocketProvider,
+    });
+  }, [activeRpc]);
+
+  return (
+    <CustomRpcContext.Provider value={customRpcContextValue}>
+      <WagmiConfig client={client}>
+        {/* @ts-ignore */}
+        <ProviderWeb3
+          pollingInterval={1200}
+          defaultChainId={dynamics.defaultChain}
+          supportedChainIds={dynamics.supportedChains}
+          rpc={activeRpc}
+          walletconnectProjectId={dynamics.walletconnectProjectId}
+        >
+          {children}
+        </ProviderWeb3>
+      </WagmiConfig>
+    </CustomRpcContext.Provider>
+  );
+};
 
 export default Web3Provider;
