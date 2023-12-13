5e1122f0ee4eec65b2b65295bbf51e9cfb9c2152
Evgeny Taktarov <je.mouth@gmail.com>
2023-12-13 15:41:46 +0700

    test: rewards unit test

---
+++ packages/sdk/src/rewards/__tests__/rewards.test.ts
@@ -0,0 +1,93 @@
+/* eslint-disable jest/no-conditional-expect */
+import { describe, test } from '@jest/globals';
+import { expectSDKModule } from '../../../tests/utils/expect/expect-sdk-module.js';
+import { LidoSDKRewards } from '../../index.js';
+
+import { useRewards } from '../../../tests/utils/fixtures/use-rewards.js';
+import { useAccount } from '../../../tests/utils/fixtures/use-wallet-client.js';
+
+import {
+  RewardsSnapshotType,
+  expectRewardsResult,
+  expectRewardsSnapshot,
+} from '../../../tests/utils/expect/expect-rewards-snapshot.js';
+
+const HOLESKY = 17000;
+const REWARDS_ADDRESS = '0x386BB4957595Df94c8F44A4f8fF99c9fef57017f';
+
+const REWARDS_SNAPSHOTS_PARAMS: RewardsSnapshotType[] = [
+  {
+    chain: HOLESKY,
+    params: {
+      address: REWARDS_ADDRESS,
+      from: { block: 452867n },
+      to: { block: 454130n },
+    },
+  },
+  {
+    chain: HOLESKY,
+    params: {
+      address: REWARDS_ADDRESS,
+      from: { timestamp: 1701858240n },
+      to: { timestamp: 1702048580n },
+      includeZeroRebases: false,
+    },
+  },
+  {
+    chain: HOLESKY,
+    params: {
+      address: REWARDS_ADDRESS,
+      back: { blocks: 10000n },
+      to: { block: 152174n },
+      includeOnlyRebases: false,
+    },
+  },
+  {
+    chain: HOLESKY,
+    params: {
+      address: REWARDS_ADDRESS,
+      back: { blocks: 467245n - 466400n },
+      to: { block: 467245n },
+      includeOnlyRebases: true,
+    },
+  },
+  {
+    chain: HOLESKY,
+    params: {
+      address: REWARDS_ADDRESS,
+      back: { blocks: 10000n },
+      to: { block: 70000n },
+      includeZeroRebases: false,
+    },
+  },
+  {
+    chain: HOLESKY,
+    params: {
+      address: REWARDS_ADDRESS,
+      back: { blocks: 10000n },
+      to: { block: 70000n },
+      includeZeroRebases: true,
+    },
+  },
+];
+
+describe('LidoSDKRewards', () => {
+  const rewards = useRewards({ useDirectRpc: true });
+  const { address } = useAccount();
+
+  test('is proper module', () => {
+    expectSDKModule(LidoSDKRewards);
+  });
+
+  test('getting rewards from chain works', async () => {
+    const result = await rewards.getRewardsFromChain({
+      address,
+      back: { days: 2n },
+    });
+    expectRewardsResult(result, { address, back: { days: 2n } });
+  });
+
+  test.each(REWARDS_SNAPSHOTS_PARAMS)('rewards snapshot $#', async (params) => {
+    await expectRewardsSnapshot(params, rewards);
+  });
+});
---
+++ packages/sdk/tests/utils/expect/expect-rewards-snapshot.ts
@@ -0,0 +1,128 @@
+import { expect } from '@jest/globals';
+import {
+  GetRewardsFromChainOptions,
+  GetRewardsFromChainResult,
+  GetRewardsFromSubgraphResult,
+  LidoSDKRewards,
+  invariant,
+} from '../../../src/index.js';
+import { useTestsEnvs } from '../fixtures/use-test-envs.js';
+import {
+  expectAlmostEqualBn,
+  expectNonNegativeBn,
+  expectPositiveBn,
+} from './expect-bn.js';
+
+export type RewardsSnapshotType = {
+  chain: number;
+  params: GetRewardsFromChainOptions;
+};
+
+export const expectRewardsSnapshot = async (
+  { chain, params }: RewardsSnapshotType,
+  sdk: LidoSDKRewards,
+) => {
+  const { chainId, subgraphUrl } = useTestsEnvs();
+  expect(chainId).toBe(chain);
+  const chainRewards = await sdk.getRewardsFromChain(params);
+  expectRewardsResult(chainRewards, params);
+  expect(chainRewards).toMatchSnapshot('-chain');
+
+  if (subgraphUrl) {
+    const subgraphRewards = await sdk.getRewardsFromSubgraph({
+      ...params,
+      getSubgraphUrl: () => ({
+        url: subgraphUrl,
+      }),
+    });
+    expectRewardsResult(subgraphRewards, params);
+    expect(subgraphRewards).toMatchSnapshot('--graph');
+
+    // match for common initial values
+    expect({
+      ...subgraphRewards,
+      lastIndexedBlock: null,
+      rewards: null,
+    }).toEqual({
+      ...chainRewards,
+      rewards: null,
+      lastIndexedBlock: null,
+    });
+
+    // match rewards
+    for (let index = 0; index < subgraphRewards.rewards.length; index++) {
+      const subgraphReward = subgraphRewards.rewards[index];
+      const chainReward = chainRewards.rewards[index];
+
+      invariant(subgraphReward, 'must have reward');
+      invariant(chainReward, 'must have reward');
+
+      expect({
+        ...subgraphReward,
+        apr: null,
+        change: null,
+        originalEvent: null,
+      }).toEqual({
+        ...chainReward,
+        apr: null,
+        change: null,
+        originalEvent: null,
+      });
+      expectAlmostEqualBn(subgraphReward.change, chainReward.change);
+      if (subgraphReward?.type === 'rebase') {
+        invariant(subgraphReward.apr, 'must have apr');
+        invariant(chainReward.apr, 'must have apr');
+        expect(subgraphReward.apr).toBeCloseTo(chainReward.apr);
+      }
+    }
+  }
+};
+
+// eslint-disable-next-line func-style
+function isSubgraphResult(
+  result: GetRewardsFromChainResult | GetRewardsFromSubgraphResult,
+): result is GetRewardsFromSubgraphResult {
+  return 'lastIndexedBlock' in result;
+}
+
+export const expectRewardsResult = (
+  result: GetRewardsFromChainResult | GetRewardsFromSubgraphResult,
+  params: GetRewardsFromChainOptions,
+) => {
+  const isSubgraph = isSubgraphResult(result);
+  expectNonNegativeBn(result.baseBalance);
+  expectNonNegativeBn(result.baseBalanceShares);
+  expectNonNegativeBn(result.totalRewards);
+  expectPositiveBn(result.fromBlock);
+  expectPositiveBn(result.toBlock);
+  expect(result.baseShareRate).toBeGreaterThan(0);
+  if (isSubgraph) {
+    expectPositiveBn(result.lastIndexedBlock);
+  }
+
+  let rewardsSum = 0n;
+  for (const reward of result.rewards) {
+    expect(reward.type).toMatch(
+      /^((submit)|(withdrawal)|(rebase)|(transfer_in)|(transfer_out))$/,
+    );
+    if (params.includeOnlyRebases) {
+      expect(reward.type).toBe('rebase');
+    }
+    if (params.includeZeroRebases === false && reward.type === 'rebase') {
+      expectPositiveBn(reward.change);
+    }
+    if (reward.type === 'rebase') {
+      rewardsSum += reward.change;
+      expect(reward.changeShares).toBe(0n);
+      expect(reward.apr).toBeGreaterThan(0);
+    }
+    expectNonNegativeBn(reward.balance);
+    expectNonNegativeBn(reward.balanceShares);
+    expectNonNegativeBn(reward.change);
+    expectNonNegativeBn(reward.changeShares);
+    expect(reward.shareRate).toBeGreaterThan(0);
+    expectNonNegativeBn(reward.changeShares);
+    expect(reward.originalEvent).toBeDefined();
+  }
+  expect(rewardsSum).toBe(result.totalRewards);
+};
