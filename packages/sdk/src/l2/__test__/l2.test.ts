17eef9c45329e3ea20e6bf4a78cdcc80e291c274
Evgeny Taktarov <je.mouth@gmail.com>
2024-09-30 17:16:03 +0700

    test: l2 wrap tests

--- packages/sdk/src/l2/__test__/l2.test.ts
+++ packages/sdk/src/l2/__test__/l2.test.ts
@@ -1,4 +1,4 @@
-import { beforeAll, describe, test } from '@jest/globals';
+import { beforeAll, describe, expect, jest, test } from '@jest/globals';
 import { expectSDKModule } from '../../../tests/utils/expect/expect-sdk-module.js';
 import {
   useL2,
@@ -13,57 +13,209 @@ import { expectERC20Wallet } from '../../../tests/utils/expect/expect-erc20-wall
 import { useAccount } from '../../../tests/utils/fixtures/use-wallet-client.js';
 import { getContract } from 'viem';
 import { bridgedWstethAbi } from '../abi/brigedWsteth.js';
+import { expectAddress } from '../../../tests/utils/expect/expect-address.js';
+import { expectContract } from '../../../tests/utils/expect/expect-contract.js';
+import {
+  expectAlmostEqualBn,
+  expectNonNegativeBn,
+} from '../../../tests/utils/expect/expect-bn.js';
+import { testSpending } from '../../../tests/utils/test-spending.js';
+import { expectTxCallback } from '../../../tests/utils/expect/expect-tx-callback.js';
+import {
+  expectPopulatedTx,
+  expectPopulatedTxToRun,
+} from '../../../tests/utils/expect/expect-populated-tx.js';
+
+const prepareL2Wsteth = async () => {
+  const l2 = useL2();
+  const account = useAccount();
+  const { testClient } = useTestL2RpcProvider();
+  const wstethAddress = await l2.wsteth.contractAddress();
+
+  const wstethImpersonated = getContract({
+    abi: bridgedWstethAbi,
+    address: wstethAddress,
+    client: testClient,
+  });
+
+  const bridge = await wstethImpersonated.read.bridge();
+
+  await testClient.setBalance({
+    address: account.address,
+    value: 100000000000000n,
+  });
+
+  await testClient.setBalance({
+    address: bridge,
+    value: 100000000000000n,
+  });
+
+  await testClient.request({
+    method: 'evm_addAccount' as any,
+    params: [bridge, 'pass'],
+  });
+
+  await testClient.request({
+    method: 'personal_unlockAccount' as any,
+    params: [bridge, 'pass'],
+  });
+
+  await wstethImpersonated.write.bridgeMint([account.address, 2000n], {
+    account: bridge,
+    chain: testClient.chain,
+  });
+};
 
 describe('LidoSDKL2', () => {
+  const l2 = useL2();
+  const account = useAccount();
+  beforeAll(async () => {
+    await prepareL2Wsteth();
+  });
+
   test('is correct module', () => {
     expectSDKModule(LidoSDKL2);
   });
+
+  test('has correct address', async () => {
+    const address = await l2.contractAddress();
+    const stethAddress = await l2.core.getL2ContractAddress(
+      LIDO_L2_CONTRACT_NAMES.steth,
+    );
+    expectAddress(address, stethAddress);
+  });
+
+  test('has contract', async () => {
+    const stethAddress = await l2.core.getL2ContractAddress(
+      LIDO_L2_CONTRACT_NAMES.steth,
+    );
+    const contract = await l2.getContract();
+    expectContract(contract, stethAddress);
+  });
+
+  test('get allowance', async () => {
+    const allowance = await l2.getWstethForWrapAllowance(account.address);
+    expectNonNegativeBn(allowance);
+
+    const contractAddress = await l2.contractAddress();
+    const altAllowance = await l2.wsteth.allowance({
+      account: account.address,
+      to: contractAddress,
+    });
+    expect(allowance).toEqual(altAllowance);
+  });
 });
 
-describe('LidoSDKL2Wsteth & LidoSDKL2Steth', () => {
+describe('LidoSDKL2 wrap', () => {
+  const l2 = useL2();
+  const account = useAccount();
+
+  const value = 100n;
+
+  beforeAll(prepareL2Wsteth);
+
+  testSpending('set allowance', async () => {
+    const mock = jest.fn();
+    const tx = await l2.approveWstethForWrap({ value, callback: mock });
+    expectTxCallback(mock, tx);
+    await expect(l2.getWstethForWrapAllowance(account)).resolves.toEqual(value);
+  });
+
+  testSpending('wrap populate', async () => {
+    const stethAddress = await l2.core.getL2ContractAddress(
+      LIDO_L2_CONTRACT_NAMES.steth,
+    );
+    const tx = await l2.wrapWstethToStethPopulateTx({ value });
+    expectAddress(tx.to, stethAddress);
+    expectAddress(tx.from, account.address);
+    expectPopulatedTx(tx, undefined);
+    await expectPopulatedTxToRun(tx, l2.core.rpcProvider);
+  });
+
+  testSpending('wrap simulate', async () => {
+    const stethAddress = await l2.core.getL2ContractAddress(
+      LIDO_L2_CONTRACT_NAMES.steth,
+    );
+    const tx = await l2.wrapWstethToStethSimulateTx({ value });
+    expectAddress(tx.address, stethAddress);
+  });
+
+  testSpending('wrap wsteth to steth', async () => {
+    const stethValue = await l2.steth.convertToSteth(value);
+    const stethBalanceBefore = await l2.steth.balance(account.address);
+    const wstethBalanceBefore = await l2.wsteth.balance(account.address);
+    const mock = jest.fn();
+    const tx = await l2.wrapWstethToSteth({ value, callback: mock });
+    expectTxCallback(mock, tx);
+    const stethBalanceAfter = await l2.steth.balance(account.address);
+    const wstethBalanceAfter = await l2.wsteth.balance(account.address);
+
+    const stethDiff = stethBalanceAfter - stethBalanceBefore;
+    const wstethDiff = wstethBalanceAfter - wstethBalanceBefore;
+
+    expectAlmostEqualBn(stethDiff, stethValue);
+    expectAlmostEqualBn(wstethDiff, -value);
+
+    expect(tx.result).toBeDefined();
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const result = tx.result!;
+    expectAlmostEqualBn(result.stethReceived, stethDiff);
+    expect(result.wstethWrapped).toEqual(-wstethDiff);
+
+    await expect(
+      l2.getWstethForWrapAllowance(account.address),
+    ).resolves.toEqual(0n);
+  });
+
+  testSpending('unwrap steth populate', async () => {
+    const stethAddress = await l2.core.getL2ContractAddress(
+      LIDO_L2_CONTRACT_NAMES.steth,
+    );
+    const tx = await l2.unwrapPopulateTx({ value });
+    expectAddress(tx.to, stethAddress);
+    expectAddress(tx.from, account.address);
+    expectPopulatedTx(tx, undefined);
+    await expectPopulatedTxToRun(tx, l2.core.rpcProvider);
+  });
+
+  testSpending('unwrap steth simulate', async () => {
+    const stethAddress = await l2.core.getL2ContractAddress(
+      LIDO_L2_CONTRACT_NAMES.steth,
+    );
+    const tx = await l2.unwrapSimulateTx({ value });
+    expectAddress(tx.address, stethAddress);
+  });
+
+  testSpending('unwrap', async () => {
+    const stethValue = await l2.steth.convertToSteth(value);
+    const stethBalanceBefore = await l2.steth.balance(account.address);
+    const wstethBalanceBefore = await l2.wsteth.balance(account.address);
+    const mock = jest.fn();
+    const tx = await l2.unwrap({ value: stethValue, callback: mock });
+    expectTxCallback(mock, tx);
+    const stethBalanceAfter = await l2.steth.balance(account.address);
+    const wstethBalanceAfter = await l2.wsteth.balance(account.address);
+
+    const stethDiff = stethBalanceAfter - stethBalanceBefore;
+    const wstethDiff = wstethBalanceAfter - wstethBalanceBefore;
+
+    expectAlmostEqualBn(stethDiff, -stethValue);
+    expectAlmostEqualBn(wstethDiff, value);
+
+    expect(tx.result).toBeDefined();
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const { stethUnwrapped, wstethReceived } = tx.result!;
+    expectAlmostEqualBn(stethUnwrapped, -stethDiff);
+    expect(wstethReceived).toEqual(wstethDiff);
+  });
+});
+
+describe('LidoSDKL2Wsteth', () => {
   const l2 = useL2();
   const l2Rpc = useL2Rpc();
-  const account = useAccount();
-  const { testClient } = useTestL2RpcProvider();
 
   beforeAll(async () => {
-    const wstethAddress = await l2.wsteth.contractAddress();
-
-    const wstethImpersonated = getContract({
-      abi: bridgedWstethAbi,
-      address: wstethAddress,
-      client: testClient,
-    });
-
-    const bridge = await wstethImpersonated.read.bridge();
-
-    await testClient.setBalance({
-      address: account.address,
-      value: 100000000000000n,
-    });
-
-    await testClient.setBalance({
-      address: bridge,
-      value: 100000000000000n,
-    });
-
-    await testClient.request({
-      method: 'evm_addAccount' as any,
-      params: [bridge, 'pass'],
-    });
-
-    await testClient.request({
-      method: 'personal_unlockAccount' as any,
-      params: [bridge, 'pass'],
-    });
-
-    await wstethImpersonated.write.bridgeMint([account.address, 2000n], {
-      account: bridge,
-      chain: testClient.chain,
-    });
-
-    await l2.approveWstethForWrap({ value: 1000n, account });
-    await l2.wrapWstethToSteth({ value: 1000n, account });
+    await prepareL2Wsteth();
   });
 
   // wstETH erc20 tests
@@ -80,6 +232,19 @@ describe('LidoSDKL2Wsteth & LidoSDKL2Steth', () => {
     constructedWithWeb3Core: l2.wsteth,
     isL2: true,
     constructedWithRpcCore: l2Rpc.wsteth,
+  });
+});
+
+describe('LidoSDKL2Steth', () => {
+  const l2 = useL2();
+  const l2Rpc = useL2Rpc();
+  const account = useAccount();
+
+  beforeAll(async () => {
+    await prepareL2Wsteth();
+
+    await l2.approveWstethForWrap({ value: 1000n, account });
+    await l2.wrapWstethToSteth({ value: 1000n, account });
   });
 
   // stETH erc20 tests
