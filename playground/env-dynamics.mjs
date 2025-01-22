1f8a4a53ed0a6a5b04f04e2ff7fd44e37cbb0f2a
DiRaiks <t.fane@mail.ru>
2025-01-22 18:58:16 +0300

    feat: add soneuim network, update viem, remove goerli

--- playground/env-dynamics.mjs
+++ playground/env-dynamics.mjs
@@ -1,14 +1,19 @@
 /** @type Record<number,string> */
 export const rpcProviderUrls = {
   1: process.env[`RPC_PROVIDER_URL_1`],
-  5: process.env[`RPC_PROVIDER_URL_5`],
   17000: process.env[`RPC_PROVIDER_URL_17000`],
   11155111: process.env[`RPC_PROVIDER_URL_11155111`],
+  // Optimism
+  10: process.env[`RPC_PROVIDER_URL_10`],
+  // OP sepolia
+  11155420: process.env[`RPC_PROVIDER_URL_11155420`],
+  // Soneium Minato
+  1946: process.env[`RPC_PROVIDER_URL_1946`],
 };
 /** @type number */
 export const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10) || 17000;
 /** @type number[] */
 export const supportedChains = process.env?.SUPPORTED_CHAINS?.split(',').map(
   (chainId) => parseInt(chainId, 10),
-) ?? [1, 5, 17000, 11155111];
+) ?? [17000, 11155111];
 export const walletconnectProjectId = process.env.WALLETCONNECT_PROJECT_ID;
