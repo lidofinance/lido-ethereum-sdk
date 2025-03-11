8be13726b7be0bf0345f3f49ef2bc8a5ae2904df
alx-khramov <alx.khramov@gmail.com>
2025-03-11 20:50:34 +0300

    fix: export unichain definition
    Signed-off-by: alx-khramov <alx.khramov@gmail.com>

---
+++ packages/sdk/src/common/chains/index.ts
@@ -0,0 +1,2 @@
+export * from './unichain.js';
+export * from './unichain-sepolia.js';
--- packages/sdk/src/common/index.ts
+++ packages/sdk/src/common/index.ts
@@ -1,2 +1,3 @@
 export * from './constants.js';
 export * from './utils/sdk-error.js';
+export * from './chains/index.js';
